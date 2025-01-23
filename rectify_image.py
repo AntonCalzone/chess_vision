c922dfdd1937dbfa8dce25e9d1b4fa3460417c9d
anton <anton.c1998@gmail.com>
2025-01-23 21:56:25 +0100

    Add black border on top of image to make sure not cropped when warping

--- rectify_image.py
+++ rectify_image.py
@@ -1,53 +1,19 @@
-# import cv2 as cv
-# import numpy as np
-
-
-# # Followed OpenCV tutorial for undistorting images
-# # https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html
-# im_path = '/Users/anton/Documents/Code/chess_vision/chess_vision/data/Chess Pieces.v23-raw.yolov8/test/images/0b47311f426ff926578c9d738d683e76_jpg.rf.0b55f43ac16aa65c889558d8ea757072.jpg'
-
-# # Find image and world points
-# criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# objp = np.zeros((6*7, 3), np.float32)
-# objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
-# obj_points = []
-# img_points = []
-
-# img = cv.imread(im_path)
-# gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-# ret, corners = cv.findChessboardCorners(gray_img, (7,6), None)
-# if ret:
-#     obj_points.append(objp)
-#     corners2 = cv.cornerSubPix(gray_img, corners, (11, 11), (-1, -1), criteria)
-#     img_points.append(corners2)
-
-#     # cv.drawChessboardCorners(img, (7, 6), corners2, ret)
-#     # cv.imshow('img', img)
-#     # cv.waitKey(5000)
-
-# # Get camera matrix
-# ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(obj_points, img_points, gray_img.shape[::-1], None, None)
-
-# # Undistort image
-# img = cv.imread(im_path)
-# h, w = img.shape[:2]
-# new_camera_matrix, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
-# undist_img = cv.undistort(img, mtx, dist, None, new_camera_matrix)
-# x, y, w, h = roi 
-# # undist_img = undist_img[y:y+h, x:x+w]
-# cv.imshow('Undistorted', undist_img)
-# cv.waitKey(5000)
-
-
 import cv2
 import numpy as np
 
-# Load the image
+# Load the image, resize from (w,h) -> (w,h+offset) and make new pixels black
+# This is to make sure that no part of the image is cropped in upper corner
 image_path = '/Users/anton/Documents/Code/chess_vision/chess_vision/data/Chess Pieces.v23-raw.yolov8/test/images/0b47311f426ff926578c9d738d683e76_jpg.rf.0b55f43ac16aa65c889558d8ea757072.jpg'
 image = cv2.imread(image_path)
+height, width = image.shape[:2]
+offset = 100
+new_height = height + offset
+resized_image = np.zeros((new_height, width, 3), dtype=np.uint8)
+resized_image[offset:, :] = image
+
 
 # Convert to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
 
 # Detect corners of the chessboard
 ret, corners = cv2.findChessboardCorners(gray, (7, 7), None)
@@ -68,6 +34,7 @@ if ret:
 
     # Define the desired output points to make the chessboard square
     # Keeping the board square but within the original image size
+    height, width = resized_image.shape[:2]
     width = int(np.linalg.norm(top_right - top_left))
     height = int(np.linalg.norm(bottom_left - top_left))
     pts2 = np.float32([
@@ -81,14 +48,11 @@ if ret:
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
 
     # Perform the perspective transformation, keeping the original image size
-    transformed = cv2.warpPerspective(image, matrix, (image.shape[1], image.shape[0]))
-
-    print(f"Size original image: {image.shape}")
-    print(f"Size transformed image: {transformed.shape}")
+    transformed = cv2.warpPerspective(resized_image, matrix, (image.shape[1], image.shape[0]))
 
     # Save or display the result
     cv2.imshow('Transformed Chessboard', transformed)
-    cv2.imshow('Original Chessboard', image)
+    # cv2.imshow('Original Chessboard', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 else:
